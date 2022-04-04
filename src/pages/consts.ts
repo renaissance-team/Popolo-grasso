30cb95882f098abbfcd3df5d47a4c5107875f860
Максим Водянов <max.post@bk.ru>
2022-04-04 22:49:21 +0300

    #6: review fix

--- src/pages/consts.ts
+++ src/pages/consts.ts
@@ -1,14 +1,12 @@
 // eslint-disable-next-line import/prefer-default-export
-export const ROUTES = {
-  HOME: '/',
-  AUTH: '/auth',
-  REGISTRATION: '/registration',
-  PROFILE: '/profile',
-  GAME: '/game',
-  SCORES: '/scores',
-  FORUM: '/forum',
-  ERRORS: {
-    404: '/404',
-    500: '/404',
-  },
-};
+export enum ROUTES {
+  HOME = '/',
+  AUTH = '/auth',
+  REGISTRATION = '/registration',
+  PROFILE = '/profile',
+  GAME = '/game',
+  SCORES = '/scores',
+  FORUM = '/forum',
+  ERROR404 = '404',
+  ERROR500 = '500',
+}
