227d4bf140ae7ba925358e601bd57ae32e5d9930
Виктория Григорьева <vgrigorieva@studio-tg.ru>
2022-04-19 23:12:37 +0300

    #0: fix ts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'yarn test --bail --findRelatedTests --watchAll=false'],
-  '*.scss': ['stylelint --fix'],
-  '*.{ts,tsx}': () => ['tsc -p tsconfig.json --noEmit'],
+  '*.{scss, css, sass}': ['stylelint --fix'],
 };
