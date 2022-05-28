a23dfa73fcf118e0a476809b93217a23123d6283
max vodyanov <max.post@bk.ru>
2022-05-28 22:46:38 +0300

    #67: oath authorization (#25)
    * #67: oath authorization
    
    * #67: review fix

--- packages/client/src/pages/Login/Auth.tsx
+++ packages/client/src/pages/Login/Auth.tsx
@@ -1,31 +1,47 @@
 import React from 'react';
 import {AnyAction} from 'redux';
 import {Link, useNavigate} from 'react-router-dom';
-import Block from '@/components/Block/Block';
 import {useDispatch} from 'react-redux';
-import {signIn} from '@/store/auth/actions';
+import Block from '@/components/Block/Block';
+import {getOAuthServiceId, signIn} from '@/store/auth/actions';
 import {useAppSelector} from '@/utils';
 import Button, {EButtonView} from '../../components/Button/Button';
 import {ROUTES} from '../consts';
 import Form, {TFormResponse} from '../../components/Form/Form';
 
+import styles from './login.module.scss';
+
 const initialFormData = [
   {name: 'login', label: 'Логин'},
   {
-    name: 'password', label: 'Пароль', type: 'password',
+    name: 'password',
+    label: 'Пароль',
+    type: 'password',
   },
 ];
 
 export default function Auth() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   const {loading} = useAppSelector((state) => state.auth);
   const formAction = (data: TFormResponse) => {
     dispatch(signIn({data, redirectFn: () => navigate(ROUTES.HOME)}) as unknown as AnyAction);
   };
+
+  const oauthHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    // todo 'as unknown as AnyAction' выглядит странно
+    dispatch(getOAuthServiceId(window.location.href) as unknown as AnyAction);
+  };
+
   return (
     <Block title="Вход">
       <Form initialData={initialFormData} onSubmit={formAction} loading={loading}>
+        <Button type="button" onClick={(event) => oauthHandler(event)} className={styles.oAuth}>
+          Войти с помощью Яндекс.ID
+        </Button>
         <Button type="submit">Войти</Button>
         <Link to={ROUTES.REGISTRATION}>
           <Button view={EButtonView.transparent}>Зарегистритроваться</Button>
