9fd15d333c27e153d0ab8df35969ff84d58980a9
Виктория Григорьева <vgrigorieva@studio-tg.ru>
2022-06-05 15:51:33 +0300

    #82: обновлен метод получение темы юзера

--- packages/server/db/theme.ts
+++ packages/server/db/theme.ts
@@ -21,6 +21,22 @@ export const getUserTheme = (host: string, port: number, user_id: number) => new
 });
 
 // eslint-disable-next-line max-len
+export const createUserTheme = (host: string, port: number, user_id: number) => new Promise<SelectedUserThemeType[]>((resolve, reject) => {
+  connect(host, port).query(
+    `INSERT INTO popolo.user_theme (theme_id, user_id)
+    VALUES ((SELECT MIN(theme_id) FROM popolo.theme), $1)
+    RETURNING *`,
+    [user_id],
+    (error: any, results: {rows: SelectedUserThemeType[]}) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(results.rows);
+    }
+  );
+});
+
+// eslint-disable-next-line max-len
 export const setTheme = (host: string, port: number, theme: UserThemeType) => new Promise<UserThemeResponseType[]>((resolve, reject) => {
   const {theme_id, device, user_id} = theme;
   connect(host, port).query(
