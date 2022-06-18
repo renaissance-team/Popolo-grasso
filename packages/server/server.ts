94cefdef2fdaedc10b5886472950f72263cf8e07
Виктория Григорьева <vgrigorieva@studio-tg.ru>
2022-06-18 18:54:15 +0300

    #85: csp

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
+import {expressCspHeader, NONCE, SELF} from 'express-csp-header';
 import {addApi} from './api';
 import serverRenderMiddleware from './render/server-render-middleware';
 
@@ -10,6 +11,18 @@ const app = express();
 
 app.use(jsonParser);
 app.use(urlParser);
+
+app.use(
+  expressCspHeader({
+    directives: {
+      'default-src': [SELF, 'ya-praktikum.tech'],
+      'script-src': [NONCE],
+      'style-src': [NONCE, 'fonts.googleapis.com'],
+      'font-src': ['fonts.googleapis.com', 'fonts.gstatic.com'],
+      'worker-src': [NONCE],
+    },
+  })
+);
 
 // в production раздавать статику через Nginx или CDN
 app.use(express.static(path.resolve(__dirname, '../client')));
